cc9c099d46174f3243a5e777908ed6fec2f51871
Raz Bracha <100145330+RazBracha@users.noreply.github.com>
2024-01-15 13:44:33 +0200

    Update LobbyPage.js

--- src/LobbyPage.js
+++ src/LobbyPage.js
@@ -4,8 +4,10 @@ import React, { useEffect, useState } from 'react';
 
 function LobbyPage() {
 
-  const server = "https://onlinecodingweb-production.up.railway.app";  // Railway server
+  //const server = "https://onlinecodingweb-production.up.railway.app";  // Railway server
   // const server = "http://localhost:5000";
+    const server ="https://online-coding-web-gg9w.vercel.app/";
+
 
   const [codeBlocks, setCodeBlocks] = useState([]);
 
